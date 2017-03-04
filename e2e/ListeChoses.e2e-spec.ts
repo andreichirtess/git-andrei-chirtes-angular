f6f2c8dcb57dae74bf6c93aef6e7fe5c85695380
AlexD <alxdmr2@yahoo.fr>
2017-03-04 21:37:34 +0100

    tests

--- e2e/ListeChoses.e2e-spec.ts
+++ e2e/ListeChoses.e2e-spec.ts
@@ -1,18 +1,20 @@
-import {browser, element, by, protractor } from "protractor";
+import {browser, element, by, protractor, ElementArrayFinder, ElementFinder} from "protractor";
 
-describe('Premiers tests', function () {
-    let inputNewTodo;
+describe('Premiers tests', () => {
+    let inputNewTodo    : ElementFinder;
+    let toggleAll       : ElementFinder;
+    let itemInputsCheck : ElementArrayFinder;
+    let items           : ElementArrayFinder;
 
-    beforeEach(function () {
+    beforeEach( () => {
         browser.get('');
-        inputNewTodo = element(by.css('input.new-todo'));
+        inputNewTodo    = element( by.css('input.new-todo') );
+        toggleAll       = element( by.css("section.main > input.toggle-all") );
+        itemInputsCheck = element.all( by.css("item-chose input.toggle") );
+        items           = element.all( by.css( "item-chose" ) );
     });
 
-    it('should display correct title', function () {
-        expect(browser.getTitle()).toEqual('Liste BSHM');
-    });
-
-    it('should add 3 new item', function() {
+    it('should add 3 new item', () => {
         inputNewTodo.sendKeys( "toto" );
         browser.actions().sendKeys( protractor.Key.ENTER ).perform();
         inputNewTodo.sendKeys( "titi" );
@@ -20,15 +22,73 @@ describe('Premiers tests', function () {
         inputNewTodo.sendKeys( "tata" );
         browser.actions().sendKeys( protractor.Key.ENTER ).perform();
 
-        let items = element.all( by.css( "item-chose" ) );
+        itemInputsCheck = element.all( by.css("item-chose input.toggle") );
+        items           = element.all( by.css( "item-chose" ) );
+
         expect(items.count()).toEqual(3);
 
-        let cases = element.all( by.css("item-chose input.toggle") );
-        cases.each( c => {
+        itemInputsCheck.each( c => {
             expect( c.isSelected() ).toBe( false );
         });
 
-        cases.click();
 
     });
+    it('toggleAll click=> all items are checked', () => {
+        toggleAll.click();
+        itemInputsCheck.each( c => {
+            expect( c.isSelected() ).toBe( true );
+        });
+    });
+    it('item 0 click => toggleAll becomes unchecked', () => {
+        itemInputsCheck.first().click();
+        expect( toggleAll.isSelected() ).toBe(false);
+    });
+    it('toggleAll click=> all items are checked', () => {
+        toggleAll.click();
+        itemInputsCheck.each( c => {
+            expect( c.isSelected() ).toBe( true );
+        });
+    });
+    it('The li root of checked items have the class "completed"', () => {
+        let lis = element.all( by.css("ul.todo-list > li.completed") );
+        expect( lis.count() ).toEqual(3);
+    });
+    it('item 0 click => toggleAll becomes unchecked', () => {
+        itemInputsCheck.first().click();
+        expect( toggleAll.isSelected() ).toBe(false);
+    });
+    it('item 0 click => toggleAll becomes checked', () => {
+        itemInputsCheck.first().click();
+        expect( toggleAll.isSelected() ).toBe(true);
+    });
 });
+
+describe("Edition d'items", () => {
+    let labelFirstItem: ElementFinder;
+    let inputNewTodo    : ElementFinder;
+    let items           : ElementArrayFinder;
+
+    beforeEach( () => {
+        browser.get('');
+        labelFirstItem  = element( by.css(`item-chose .view label`) );
+        inputNewTodo    = element( by.css('input.new-todo') );
+        items           = element.all( by.css( "item-chose" ) );
+    });
+
+    it('click on label should make input appears and text should be kept.', () => {
+        let txtLabel = labelFirstItem.getText();
+        browser.actions().doubleClick( labelFirstItem ).perform();
+
+        let input = element( by.css(`item-chose form input`) );
+        expect( input.getAttribute("value") ).toEqual( txtLabel );
+
+        input.sendKeys( " Bob" );
+        let strInput = input.getAttribute( "value" );
+
+        browser.actions().sendKeys( protractor.Key.ENTER ).perform();
+
+        labelFirstItem  = element( by.css(`item-chose .view label`) );
+        expect( labelFirstItem.getText() ).toEqual( strInput );
+    });
+});
+
--- ts/components/ListeChoses.ts
+++ ts/components/ListeChoses.ts
@@ -63,4 +63,3 @@ export class ListeChoses implements OnInit {
         return this.choses;
     }
 }
-
